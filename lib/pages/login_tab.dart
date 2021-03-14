6276067184663eb8b47d03212c905e152154133d
Victor Gsell <victor.gsell@gmail.com>
2021-03-15 00:52:24 +0100

    Login tab layout
    Type: feat
    
    Description: Added a basic layout for login purposes in 'login_tab.dart'

--- lib/pages/login_tab.dart
+++ lib/pages/login_tab.dart
@@ -10,11 +10,46 @@ class LoginTab extends StatefulWidget {
 }
 
 class _LoginTabState extends State<LoginTab> {
+  final TextEditingController _emailController = TextEditingController()
+    ..text = "default_value";
+  final TextEditingController _pswController = TextEditingController()
+    ..text = "default_value";
+
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[],
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Center(
+          child: SizedBox(
+              width: MediaQuery.of(context).size.width / 1.5,
+              child: TextField(
+                maxLines: 1,
+                maxLength: 50,
+                readOnly: true,
+                controller: _emailController,
+                decoration: InputDecoration(
+                  icon: Icon(Icons.mail_outline_rounded),
+                  labelText: "email"
+                ),
+              )),
+        ),
+        Center(
+          child: SizedBox(
+              width: MediaQuery.of(context).size.width / 1.5,
+              child: TextField(
+                maxLines: 1,
+                maxLength: 50,
+                readOnly: true,
+                controller: _pswController,
+                decoration: InputDecoration(
+                  icon: Icon(Icons.lock_outline_rounded),
+                  labelText: "password",
+                ),
+              )),
+        ),
+        Center(child: ElevatedButton(onPressed: () {}, child: Text("Login")))
+      ],
     );
   }
 }
